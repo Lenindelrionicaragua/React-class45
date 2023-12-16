094a2c649c1a68e756ef2f10e1a3c520f5bffc5e
lenin del rio <lenindelrionicaragua@gmail.com>
2023-12-16 17:20:11 +0100

    Refactor code for better readability, conciseness and Styles2

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import CategoryListItem from "./CategoryListItem";
 
 const CategoryList = ({ categories, selectedCategory, onCategoryClick }) => {

3a7d8af1d4fc9bfff1640206755bf8782a7ebdf3
lenin del rio <lenindelrionicaragua@gmail.com>
2024-01-05 17:35:15 +0100

    update useAvatar

--- week3/prep-exercises/1-check-screen-size/my-avatar-app/src/App.js
+++ week3/prep-exercises/1-check-screen-size/my-avatar-app/src/App.js
@@ -1,60 +1,28 @@
 import React from "react";
 import useAvatar from "./useAvatar";
-import { BigHead } from "@bigheads/core";
 import "./App.css";
 
 const App = () => {
-  const { screenType } = useAvatar();
+  const { screenType, windowSize } = useAvatar();
 
-  let avatar;
-  switch (screenType) {
-    case "big":
-      avatar = (
-        <BigHead
-          accessory="shades"
-          body="chest"
-          circleColor="blue"
-          clothing="tankTop"
-          clothingColor="black"
-          eyebrows="angry"
-          eyes="wink"
-          facialHair="mediumBeard"
-          graphic="vue"
-          hair="short"
-          hairColor="black"
-          hat="none"
-          hatColor="green"
-          lashes="false"
-          lipColor="purple"
-          mask="true"
-          faceMask="true"
-          mouth="open"
-          skinTone="brown"
-          className="avatar" // Agrega una clase para aplicar estilos específicos al avatar
-        />
-      );
-      break;
-    case "small":
-      avatar = (
-        <BigHead
-          // Define propiedades para pantalla pequeña
-          className="avatar" // Agrega una clase para aplicar estilos específicos al avatar
-        />
-      );
-      break;
-    default:
-      avatar = (
-        <BigHead
-          // Define propiedades para pantalla mediana
-          className="avatar" // Agrega una clase para aplicar estilos específicos al avatar
-        />
-      );
-  }
+  const characterNames = {
+    big: "Mithi",
+    medium: "Diana",
+    small: "Mikong",
+  };
 
   return (
     <div>
       <h1>Avatar App</h1>
-      <div className="avatar-container">{avatar}</div>
+      <div className="avatar-container">
+        {useAvatar().avatar}
+        <div className="avatar-info">
+          {screenType && <p>Character: {characterNames[screenType]}</p>}
+          <p>Screen Type: {screenType}</p>
+          <p>Window Width: {windowSize.width}</p>
+          <p>Window Height: {windowSize.height}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- week3/prep-exercises/1-check-screen-size/my-avatar-app/src/useAvatar.js
+++ week3/prep-exercises/1-check-screen-size/my-avatar-app/src/useAvatar.js
@@ -1,8 +1,6 @@
-// src/useAvatar.js
 import { useState, useEffect, useDebugValue } from "react";
 import { BigHead } from "@bigheads/core";
 
-// eslint-disable-next-line
 const useAvatar = () => {
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
@@ -24,43 +22,93 @@ const useAvatar = () => {
     };
   }, []);
 
-  // Determine the screen type based on width
-  let screenType = "medium";
-  if (windowSize.width > 1000) {
-    screenType = "big";
-  } else if (windowSize.width < 700) {
-    screenType = "small";
-  }
+  const isScreenSize = (minWidth, maxWidth) => {
+    return windowSize.width >= minWidth && windowSize.width <= maxWidth;
+  };
 
-  // Use useDebugValue to display a label in React DevTools
-  useDebugValue(`Screen Type: ${screenType}`);
+  const screenType = () => {
+    if (isScreenSize(0, 700)) {
+      return "small";
+    } else if (isScreenSize(701, 1000)) {
+      return "medium";
+    } else {
+      return "big";
+    }
+  };
 
-  // Use BigHead or store it in a variable for future use
-  const avatar = (
-    <BigHead
-      accessory="shades"
-      body="chest"
-      circleColor="blue"
-      clothing="tankTop"
-      clothingColor="black"
-      eyebrows="angry"
-      eyes="wink"
-      facialHair="mediumBeard"
-      graphic="vue"
-      hair="short"
-      hairColor="black"
-      hat="none"
-      hatColor="green"
-      lashes="false"
-      lipColor="purple"
-      mask="true"
-      faceMask="true"
-      mouth="open"
-      skinTone="brown"
-    />
+  useDebugValue(
+    `Screen Type: ${screenType()}, Width: ${windowSize.width}, Height: ${
+      windowSize.height
+    }`
   );
 
-  return { screenType, avatar };
+  const Mithi = {
+    body: "breasts",
+    eyebrows: "serious",
+    eyes: "simple",
+    faceMask: false,
+    facialHair: "none",
+    hair: "long",
+    hairColor: "black",
+    lashes: false,
+    lipColor: "purple",
+    mask: false,
+    mouth: "serious",
+    skinTone: "brown",
+    className: "avatar",
+  };
+
+  const Diana = {
+    body: "breasts",
+    eyebrows: "leftLowered",
+    eyes: "leftTwitch",
+    faceMask: false,
+    faceMaskColor: "black",
+    facialHair: "none",
+    hair: "afro",
+    hairColor: "blue",
+    lashes: true,
+    lipColor: "green",
+    mask: false,
+    mouth: "lips",
+    skinTone: "light",
+    className: "avatar",
+  };
+
+  const Mikong = {
+    body: "chest",
+    eyebrows: "angry",
+    eyes: "content",
+    faceMask: false,
+    facialHair: "stubble",
+    hair: "buzz",
+    hairColor: "black",
+    lashes: false,
+    lipColor: "purple",
+    mask: false,
+    mouth: "openSmile",
+    skinTone: "light",
+    className: "avatar",
+  };
+
+  const getAvatarByScreenType = () => {
+    switch (screenType()) {
+      case "big":
+        return <BigHead {...Mithi} />;
+      case "medium":
+        return <BigHead {...Diana} />;
+      case "small":
+        return <BigHead {...Mikong} />;
+      default:
+        return null;
+    }
+  };
+
+  return {
+    screenType: screenType(),
+    windowSize,
+    avatar: getAvatarByScreenType(),
+  };
 };
 
 export default useAvatar;
